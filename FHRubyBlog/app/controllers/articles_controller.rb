6a7516aa707fdd0c1e1a6a5716f1fa177cdfcfe8
Hu Xiang <johnhu456@163.com>
2017-06-15 21:58:35 +0800

    Finish index display

--- FHRubyBlog/app/controllers/articles_controller.rb
+++ FHRubyBlog/app/controllers/articles_controller.rb
@@ -3,7 +3,22 @@ class ArticlesController < ApplicationController
 
   end
 
-  def create
-    render json: params[:articles]
+  def index
+    @articles = Article.all
   end
+
+  def show
+    @articles = Article.find(params["id"])
+  end
+
+  def create
+    @articles = Article.new(articles_params)
+    @articles.save
+    redirect_to @articles
+  end
+
+  private
+    def articles_params
+      params.require(:articles).permit(:title,:text)
+    end
 end
