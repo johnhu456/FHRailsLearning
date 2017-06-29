6ad97998b97403b9efeebd836cb660afcca74be2
Hu Xiang <johnhu456@163.com>
2017-06-29 13:12:57 +0800

    Done with delete comments

--- FHRubyBlog/app/controllers/comments_controller.rb
+++ FHRubyBlog/app/controllers/comments_controller.rb
@@ -5,6 +5,15 @@ class CommentsController < ApplicationController
     redirect_to articles_path(@articles)
   end
 
+  def destroy
+    @articles = Article.find(params[:article_id])
+    @comment = @articles.comments.find(params[:id])
+    p params
+    if @comment.destroy
+       redirect_to @articles
+    end
+  end
+
   def comment_params
     p params
     params.require(:comment).permit(:commenter,:body)
