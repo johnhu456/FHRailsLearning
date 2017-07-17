7b7a9d6d8089be1e85500f0a12638056e4ff4322
Hu Xiang <johnhu456@163.com>
2017-07-17 13:23:56 +0800

    Finish vote site part1

--- VoteSite/app/controllers/candidates_controller.rb
+++ VoteSite/app/controllers/candidates_controller.rb
@@ -16,6 +16,28 @@ class CandidatesController < ApplicationController
     end
   end
 
+  def edit
+    @candidate = Candidate.find(params[:id])
+  end
+
+  def update
+    @candidate = Candidate.find(params[:id])
+    if @candidate.update(candidate_params)
+      redirect_to candidates_path, notice: "更新资料成功！"
+    else
+       render edit
+    end
+  end
+
+  def destroy
+    @candidate = Candidate.find(params[:id])
+    if @candidate.destroy
+      redirect_to candidates_path, notice: "删除成功！"
+    else
+      render index
+    end
+  end
+
   private
   def candidate_params
     params.require(:candidate).permit(:name, :age, :party, :politics)
